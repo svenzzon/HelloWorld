e7e3b4f44c4f2a3ea09ac667007735f2d90d1770
Jonne Dahlberg <jonne.dahlberg@gmail.com>
2013-03-23 22:01:35 +0100

    First shot in c#
    Just the first sourcecode ...

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+
+namespace HelloWorld
+{
+    class Program
+    {
+        /// <summary>
+        /// First program I ever wrote in C#
+        /// Hello World !
+        /// </summary>
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World !");
+            Console.ReadLine();
+        } // end Main
+    } // end class Program
+} // end namespace HelloWorld
